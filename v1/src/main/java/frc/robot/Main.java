f33a706638a0db7b1d2e3947b8b05cdf641ce277
Andrei <andrei@notakes.com>
2024-03-04 15:30:39 -0500

    testing?

--- v1/src/main/java/frc/robot/Main.java
+++ v1/src/main/java/frc/robot/Main.java
@@ -1,7 +1,6 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-// Ryan, Mark, Andrei, Lucas, Chris, Ben
 
 // Ryan, Mark, Chris, Andrei, Lucus, Ben (Programmers)
 
