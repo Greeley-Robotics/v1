a29145bc83f4e314eddc9c65e65cf4d0f997e199
RZJournal <rzjournal@rzjournal.com>
2024-03-28 14:25:40 -0400

    Add DriveInterval, co-developed with Andrei and Lucas

--- src/main/java/frc/robot/commands/driveCommands/DriveInterval.java
+++ src/main/java/frc/robot/commands/driveCommands/DriveInterval.java
@@ -1,5 +1,34 @@
 package frc.robot.commands.driveCommands;
 
-public class DriveInterval {
+import frc.robot.subsystems.Drivetrain;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class DriveInterval extends Command {
+    private final Drivetrain m_drivetrain;
     
+    public DriveInterval(Drivetrain sub_sys) {
+        m_drivetrain = sub_sys;
+        addRequirements(sub_sys);
+        m_drivetrain.sparkMax1.set(0.0);
+    }
+
+    
+
+
+    public void initialize() {
+        m_drivetrain.sparkMax1.set(0.0);
+    }
+
+    public void execute() {
+        m_drivetrain.sparkMax1.set(0.5);
+    }
+    public void end(boolean interrupted) {
+        m_drivetrain.sparkMax1.stopMotor();
+    }
+
+     public boolean isFinished() {
+    return false;
+  }
+
 }
